559b1f550f47f92fb42200be70b74b24b7362a4e
Saad Desktop <saadsohailbajwa@gmail.com>
2023-06-05 02:50:40 +0500

    day3: we can nest components
    other learned things on google docs

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greetings() {
-    return <h1>This is my first component</h1>;
+    return <div>
+        <NestedComp/>
+        <ChildNestedComp/>
+    </div>;
 }
+
+let NestedComp = () => {
+    return <h1>Parent</h1>
+}
+
+let ChildNestedComp = () => <h3>Child</h3>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function ReactCreateEle(){
+    return React.createElement('h3',{},'this component was made by react.createElement()');
+};
+
+//root.render(<ReactCreateEle/>);
 root.render(<Greetings />);
