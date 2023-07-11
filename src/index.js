b9ade1d1530111a54eb454755c43b5912b3a75f7
Saad <saadsohailbajwa@gmail.com>
2023-07-11 14:02:05 +0500

    clean up book component and got started with javascript in html elements

--- src/index.js
+++ src/index.js
@@ -8,15 +8,46 @@ function BookList(){
         <Book/>
         <Book/>
         <Book/>
+        <Book/>
 
     </section>
 }
 
-const Book = () => <article>
-<Image/>
-<BookTitle/>
-<AuthorName/>
-</article>;
+const Book = () => {
+
+const bookTitle = 'TOO PRETTY TO DIE'
+
+return (
+<article>
+<img
+    src="/Images/book-cover.jpg"
+    alt="TOO PRETTY TO DIE"
+  />
+<h2>{bookTitle}</h2>
+<p>Willow Rose</p>
+
+<p>{5 + 6}</p>
+
+</article>
+)};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function ReactCreateEle(){
+    return React.createElement('h3',{},'this component was made by react.createElement()');
+};
+
+//root.render(<ReactCreateEle/>);
+root.render(<BookList />);
+
+
+
+
+
+
+
+/* 
 
 const Image = () => (
   <img
@@ -31,11 +62,4 @@ const AuthorName = () => {
   const inlineCss = { backgroundColor:'White', fontSize:'1rem'}
 return <p style={inlineCss}>Willow Rose</p>;}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-function ReactCreateEle(){
-    return React.createElement('h3',{},'this component was made by react.createElement()');
-};
-
-//root.render(<ReactCreateEle/>);
-root.render(<BookList />);
+*/
